199275e660f8f68f94e62abf2331859006bac239
Federico <f.fulgeri@gmail.com>
2020-12-29 22:02:10 +0100

    auth

--- database/models.py
+++ database/models.py
@@ -1,7 +1,25 @@
 from .db import db
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 
 class Movie(db.Document):
     name = db.StringField(required=True, unique=True)
     casts = db.ListField(db.StringField(), required=True)
     genres = db.ListField(db.StringField(), required=True)
+    added_by = db.ReferenceField('User') # relazione molti a uno
+
+
+class User(db.Document):
+    email = db.StringField(required=True, unique=True)
+    password = db.StringField(required=True, min_length=6)
+    movies = db.ListField(db.ReferenceField('Movie', reverse_delete_rule=db.PULL)) #relazione uno a molti
+
+    def hash_password(self):
+        self.password = generate_password_hash(self.password).decode('utf8')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+#se l'utente viene cancellato a cascata sono eliminati anche tutti i film che ha inserito
+User.register_delete_rule(Movie, 'added_by', db.CASCADE)
